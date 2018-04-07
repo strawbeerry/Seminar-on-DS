4c9d3dcc8d892d4752ce8de9d143e81a53931fcc
Lizy321 <32933043+Lizy321@users.noreply.github.com>
2018-04-07 11:30:46 +0200

    Add files via upload

--- gradient.m
+++ gradient.m
@@ -1,9 +1,47 @@
 function [ G ] = gradient( h, w )
 %GRADIENT Summary of this function goes here
 %   Detailed explanation goes here
-i = ones(2*h*(w-1)+2*w*(h-1),1);
-j = ones(2*h*(w-1)+2*w*(h-1),1);
-v = ones(2*h*(w-1)+2*w*(h-1),1);
+%i = zeros(2*h*(w-1)+2*w*(h-1),1);
+%j = zeros(2*h*(w-1)+2*w*(h-1),1);
+%v = zeros(2*h*(w-1)+2*w*(h-1),1);
+
+i = [];j = [];v = [];
+
+for session = 1:(w)
+for row = 1:(h-1)
+
+    idx = 2*((session-1)*(h-1)+row);
+    
+    i(idx-1) = idx/2;
+    i(idx) = i(idx-1);
+    j(idx-1) = h*(session-1)+row;
+    j(idx) = j(idx-1)+1;
+    v(idx-1) = 1;
+    v(idx) = -1;
+    
+end
+end
+
+for row = (1):h
+    for c = 1:(w-1)
+    
+        R = 2*((h-1)*w+(row-1)*(w-1)+c);
+        if R ==0
+            display(R);
+        end
+        i(R-1) = R/2;
+        i(R) = i(R-1);
+        j(R-1) = row+h*(c-1);
+        j(R) = j(R-1)+h;
+        v(R-1) = -1;
+        v(R) = 1;
+    end
+end
+
+%display(i);
+%display(j);
+%display(v);
+
 
 % collect triplets here
 
